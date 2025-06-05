70b7323a5cb88262e9fd3106ed7125ef214903cf
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2025-06-05 20:38:45 +0900

    :ambulance: Configure Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: './src',
+  base: '/skyrim-control-mapper',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
