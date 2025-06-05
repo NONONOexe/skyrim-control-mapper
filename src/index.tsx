6f68be1c8c4dc52d5d2221039ff2b5b44ac2377b
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2025-06-05 20:27:54 +0900

    :sparkles: Migrate to Vite and configure GitHub Pages deployment

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client';
 
 interface BindingFile {
     name: string
@@ -732,4 +734,8 @@ function App() {
     </>
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const container = document.getElementById('root');
+if (container) {
+    const root = createRoot(container);
+    root.render(<App />);
+}
